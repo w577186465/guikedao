1d0adfa1eeb6f3c78e9453f93bd960cb918d1b5c
unknown <vsuper>
2018-02-23 16:31:54 +0800

    commit

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -41,8 +41,6 @@ $app->singleton(
 	App\Exceptions\Handler::class
 );
 
-$app->register(Overtrue\LaravelWeChat\ServiceProvider::class);
-
 /*
 |--------------------------------------------------------------------------
 | Return The Application
